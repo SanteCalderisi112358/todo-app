9c0abe8cf1faaffdf03edcf3d1585b62d3f6c4ad
SanteCalderisi112358 <santecalderisi@gmail.com>
2024-02-16 16:57:20 +0100

    Creazione todo-app-BE e apertura Maven

---
+++ todo-app-BE/src/main/java/com/example/todoappBE/TodoAppBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.todoappBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppBeApplication.class, args);
+	}
+
+}
